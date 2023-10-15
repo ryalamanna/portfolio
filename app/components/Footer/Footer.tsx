35257550a0217d24222f0070dee43f91b9ff0d4a
Ryal rafter amanna <ryalamanna@gmail.com>
2023-10-15 19:59:26 +0530

    updated header and footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -2,37 +2,56 @@ import GoUp from '../GoUp/GoUp';
 import './Footer.scss';
 
 const Footer = () => {
-  return (
-    <div className="globalContainer">
-      <section className='main_footer section'>
-        <div className='first'>
-          <h2 className='font-bold'>RYAL</h2>
-          <p className="thanks font-regular">Thanks for stopping by ッ</p>
+    return (
+        <div className="globalContainer">
+            <section className="main_footer section">
+                <div className="first">
+                    <h2 className="font-bold">RYAL</h2>
+                    <p className="thanks font-regular">
+                        Thanks for stopping by ッ
+                    </p>
+                </div>
+                <div className="section">
+                    <ul>
+                        <li className="head font-bold">Link</li>
+                        <li className="font-regular">
+                            <a href="/about">About</a>
+                        </li>
+                        <li className="font-regular">
+                            <a href="/work">Work</a>
+                        </li>
+                        <li className="font-regular">
+                            <a href="/tech-stack">Tech Stack</a>
+                        </li>
+                        <li className="font-regular">
+                            <a href="/contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="section">
+                    <ul>
+                        <li className="head font-bold">Elsewhere</li>
+                        <li className="font-regular">
+                            <a href="">Email</a>
+                        </li>
+                        <li className="font-regular">
+                            <a href="">LinkedIn</a>
+                        </li>
+                        <li className="font-regular">
+                            <a href="">GitHub</a>
+                        </li>
+                        <li className="font-regular">
+                            <a href="">Twitter</a>
+                        </li>
+                        <li className="font-regular">
+                            <a href="">Discord</a>
+                        </li>
+                    </ul>
+                </div>
+                <GoUp />
+            </section>
         </div>
-        <div className='section'>
-          <ul>
-            <li className='head font-bold'>Link</li>
-            <li className='font-regular'><a href="">About</a></li>
-            <li className='font-regular'><a href="">Work</a></li>
-            <li className='font-regular'><a href="/tech-stack">Tech Stack</a></li>
-            <li className='font-regular'><a href="">Contact</a></li>
-          </ul>
-        </div>
-        <div className='section'>
-          <ul>
-              <li className='head font-bold'>Elsewhere</li>
-              <li className='font-regular'><a href="">Email</a></li>
-              <li className='font-regular'><a href="">LinkedIn</a></li>
-              <li className='font-regular'><a href="">GitHub</a></li>
-              <li className='font-regular'><a href="">Twitter</a></li>
-              <li className='font-regular'><a href="">Discord</a></li>
-          </ul>
-        </div>
-        <GoUp/>
-      </section>
-    </div>
-    
-  )
-}
+    );
+};
 
-export default Footer
+export default Footer;
--- app/components/nav/nav.jsx
+++ app/components/nav/nav.jsx
@@ -1,26 +1,58 @@
-import styles from './Nav.module.css'
+import styles from './Nav.module.css';
 import Link from 'next/link';
 const Nav = () => {
     return (
         <div className="globalContainer">
             <nav className={styles.navbar}>
-            <div className={styles.logo}><Link href="/">RYAL</Link></div>
-            <ul>
-                <li><Link href="#">About</Link></li>
-                <li><Link href="#">Work</Link></li>
-                <li><Link href="/contact">Contact</Link></li>
-                <li><Link href="/tech-stack">Tech Stack</Link></li>
-            </ul>
-            <ul className={styles.social}>
-                <li><Link href="#"><img src="/Linkedin.png" alt="" /></Link></li>
-                <li><Link href="#"><img className={styles.noInverse} src="/X.png" alt="" /></Link></li>
-                <li><Link href="#"><img src="/Github.png" alt="" /></Link></li>
-                <li><img src="/Line9.png" alt="" /></li>
-                <li><Link href=""><img src="/Sun.png" alt="" /></Link></li>
-            </ul>
+                <div className={styles.logo}>
+                    <Link href="/">RYAL</Link>
+                </div>
+                <ul>
+                    <li>
+                        <Link href="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link href="/work">Work</Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">Contact</Link>
+                    </li>
+                    <li>
+                        <Link href="/tech-stack">Tech Stack</Link>
+                    </li>
+                </ul>
+                <ul className={styles.social}>
+                    <li>
+                        <Link href="#">
+                            <img src="/Linkedin.png" alt="" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="#">
+                            <img
+                                className={styles.noInverse}
+                                src="/X.png"
+                                alt=""
+                            />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="#">
+                            <img src="/Github.png" alt="" />
+                        </Link>
+                    </li>
+                    <li>
+                        <img src="/Line9.png" alt="" />
+                    </li>
+                    <li>
+                        <Link href="">
+                            <img src="/Sun.png" alt="" />
+                        </Link>
+                    </li>
+                </ul>
             </nav>
         </div>
-    )
-}
+    );
+};
 
-export default Nav
+export default Nav;

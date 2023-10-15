6d1601e80bc47cfe5d49dbc9dd9a66a70bfc3acc
Ryal rafter amanna <ryalamanna@gmail.com>
2023-10-15 19:57:39 +0530

    api host changed

--- app/helper/axios.ts
+++ app/helper/axios.ts
@@ -2,31 +2,31 @@ import axioss from 'axios';
 
 // Create an Axios instance
 const axios = axioss.create({
-  baseURL: 'http://localhost:3000/api', // Replace with your backend API URL
+    baseURL: window.location.origin + 'api', // Replace with your backend API URL
 });
 
 // Request interceptor
 axios.interceptors.request.use(
-  (config) => {
-    // Modify the request config here (if needed)
-    return config;
-  },
-  (error) => {
-    // Handle request errors here
-    return Promise.reject(error);
-  }
+    (config) => {
+        // Modify the request config here (if needed)
+        return config;
+    },
+    (error) => {
+        // Handle request errors here
+        return Promise.reject(error);
+    }
 );
 
 // Response interceptor
 axios.interceptors.response.use(
-  (response) => {
-    // Modify the response data here (if needed)
-    return response;
-  },
-  (error) => {
-    // Handle response errors here
-    return Promise.reject(error);
-  }
+    (response) => {
+        // Modify the response data here (if needed)
+        return response;
+    },
+    (error) => {
+        // Handle response errors here
+        return Promise.reject(error);
+    }
 );
 
 export default axios;

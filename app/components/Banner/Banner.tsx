b672168ddc3863da251d331ce491eb315b0fd1b3
ryalamanna <ryalamanna@gmail.com>
2024-11-28 21:20:53 +0530

    made home page responsive

--- app/components/Banner/Banner.tsx
+++ app/components/Banner/Banner.tsx
@@ -3,6 +3,9 @@ import Styles from './Banner.module.scss'
 const Banner = () => {
   return (
     <header className={Styles.banner}>
+        <div className={Styles.rightImgPhone}>
+            <img src="/ryal.png" alt="" />
+        </div>
         <div className="left">
             <h1><span className={Styles.highlight}>I’m</span> Ryal Rafter</h1>
             <p className={Styles.subHead}>I am a dynamic full-stack developer with nearly a year of hands-on experience, proficient in crafting cutting-edge and responsive websites and web applications that seamlessly integrate functionality and design.
